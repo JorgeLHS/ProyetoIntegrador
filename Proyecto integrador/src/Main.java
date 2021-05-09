0debd2acd7892941da4c3fd99f9e1581c127fed3
JorgeLHS <70728826+JorgeLHS@users.noreply.github.com>
2021-05-09 14:13:28 -0500

    Add files via upload

--- Proyecto integrador/src/Main.java
+++ Proyecto integrador/src/Main.java
@@ -2,13 +2,42 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Main {
-    public static void main(String[]args){
+   /* public static int[] MétodoBurbuja(int[] vector)
+    {
+        int TerminoA;
+        int[] VectorOrdenado;
+        for(int i = 1; i < vector.length; i++)
+        {
+            for(int j = 0; j < vector.length-i; j++)
+            {
+                if(vector[j] > vector[j+1])
+                {
+                    TerminoA = vector[j];
+                    vector[j] = vector[j+1];
+                    vector[j+1] = TerminoA;
+                }
+            }
+        }
+        VectorOrdenado = vector;
+        return VectorOrdenado;
+    }*/
 
-        JFrame frame = new JFrame("Juego ordenamiento");
-        frame.setSize(500,500);
+    public static void main(String[]args) {
+
+        JFrame frame = new JFrame("Ordenamiento y búsqueda");
+        frame.setSize(500, 500);
         frame.setLayout(new BorderLayout());
         frame.setContentPane(new Myform().pnlMain);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE );
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
+
+       /* System.out.println("Digite el vector");
+        int vector[] = {1,4,5,3,12,23,7,6};
+
+        int VectorOrdenado[] = MétodoBurbuja(vector);
+
+        for(int i = 0; i < VectorOrdenado.length;i++)
+            System.out.println(VectorOrdenado[i]);
+    }*/
     }
 }
