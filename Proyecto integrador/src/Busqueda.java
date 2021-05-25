6d80a92c21b1e904ce59f94ab61c3e10f5ba1db7
JorgeLHS <70728826+JorgeLHS@users.noreply.github.com>
2021-05-25 10:02:10 -0500

    Add files via upload

--- Proyecto integrador/src/Busqueda.java
+++ Proyecto integrador/src/Busqueda.java
@@ -1,7 +1,12 @@
 import javax.swing.*;
+import java.security.PublicKey;
 
 public class Busqueda {
     public JPanel pnlMain4;
-    private JLabel lblConceptoBusqueda;
-    private JScrollBar scrollBar1;
+    private JTextArea conMuchaFrecueniaLosTextArea;
+
+
+    public Busqueda() {
+
+    }
 }
