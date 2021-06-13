5394e9891ba3d66a08b71074f0e42cf6e00494b8
JorgeLHS <70728826+JorgeLHS@users.noreply.github.com>
2021-06-13 13:27:41 -0500

    Add files via upload

---
+++ Proyecto integrador/src/Ordenamiento.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.AdjustmentEvent;
+import java.awt.event.AdjustmentListener;
+
+public class Ordenamiento {
+    public JPanel pnlMain3;
+    private JTextArea txtA_insercion;
+    private JTextArea txtA_Burbuja;
+    private JTextArea txtA_Seleccion;
+    private JButton btnSeleccion;
+    private JButton btnInsercion;
+    private JButton btnBurbuja;
+    private JTextArea fldOrdenamientoConcepto;
+
+
+    public Ordenamiento() {
+    }
+}
+
