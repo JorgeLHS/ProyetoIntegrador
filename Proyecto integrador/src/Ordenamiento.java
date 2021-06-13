b18882197c423c1bff4616c8b19ccaa561e176f3
JorgeLHS <70728826+JorgeLHS@users.noreply.github.com>
2021-06-13 13:27:02 -0500

    Delete Proyecto integrador directory

--- Proyecto integrador/src/Ordenamiento.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.*;
-
-public class Ordenamiento {
-    public JPanel pnlMain3;
-    private JLabel lblConceptoOdernar;
-    private JLabel lblSelección;
-    private JLabel lblInserción;
-    private JLabel lblBurbuja;
-}
